1b72027c85bc660c66a91e31320960f2b58ae7d8
Aodan O Laoghaire <olaoghaa@tcd.ie>
2017-02-18 13:26:23 +0000

    app takes from image gallery and shows location

---
+++ app/src/main/java/com/prgguru/example/MainActivity.java
@@ -0,0 +1,186 @@
+package com.prgguru.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;import android.annotation.SuppressLint;
+import android.location.Location;
+import android.media.ExifInterface;
+
+
+public class MainActivity extends Activity {
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
+    TextView Exif;
+    TextView txtSDK;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Exif = (TextView)findViewById(R.id.exif);
+
+        setContentView(R.layout.activity_main);
+        //Exif = (TextView)findViewById(R.id.exif);
+       // txtSDK = (TextView) findViewById(R.id.txtSDK);
+       /* btnSelectImage = (Button) findViewById(R.id.btnSelectImage);
+        txtUriPath = (TextView) findViewById(R.id.txtUriPath);
+        txtRealPath = (TextView) findViewById(R.id.txtRealPath);*/
+    }
+
+    public void loadImagefromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+                String realPath;
+                //realPath = RealPathUtil.getRealPathFromURI_API11to18(this, data.getData());
+                Uri selectedImage = data.getData();
+                String s = getRealPathFromURI(selectedImage);
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Location c=readGeoTagImage(s);
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                System.out.print(" path  "+filePathColumn[0]);
+               // System.out.println("Date: " + exif.get(0x0132)); //0x9003
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imgView);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+                Toast.makeText(this, "Local  ="+c, Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+
+
+    public String getRealPathFromURI(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        @SuppressWarnings("deprecation")
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    public Location readGeoTagImage(String imagePath)
+    {
+        Location loc = new Location("");
+        try {
+            ExifInterface exif = new ExifInterface(imagePath);
+            float [] latlong = new float[2] ;
+            if(exif.getLatLong(latlong)){
+                loc.setLatitude(latlong[0]);
+                loc.setLongitude(latlong[1]);
+            }
+            String date = exif.getAttribute(ExifInterface.TAG_DATETIME);
+            SimpleDateFormat fmt_Exif = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+            loc.setTime(fmt_Exif.parse(date).getTime());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return loc;
+    }
+
+
+    //Code to convert  Degrees to DMS unit
+
+    class GPS {
+        private StringBuilder sb = new StringBuilder(20);
+
+        /**
+         * returns ref for latitude which is S or N.
+         *
+         * @param latitude
+         * @return S or N
+         */
+        public String latitudeRef(final double latitude) {
+            return latitude < 0.0d ? "S" : "N";
+        }
+
+        /**
+         * returns ref for latitude which is S or N.
+         *
+         * @param //latitude
+         * @return S or N
+         */
+        public String longitudeRef(final double longitude) {
+            return longitude < 0.0d ? "W" : "E";
+        }
+
+        /**
+         * convert latitude into DMS (degree minute second) format. For instance<br/>
+         * -79.948862 becomes<br/>
+         * 79/1,56/1,55903/1000<br/>
+         * It works for latitude and longitude<br/>
+         *
+         * @param latitude could be longitude.
+         * @return
+         */
+        public final String convert(double latitude) {
+            latitude = Math.abs(latitude);
+            final int degree = (int) latitude;
+            latitude *= 60;
+            latitude -= degree * 60.0d;
+            final int minute = (int) latitude;
+            latitude *= 60;
+            latitude -= minute * 60.0d;
+            final int second = (int) (latitude * 1000.0d);
+            sb.setLength(0);
+            sb.append(degree);
+            sb.append("/1,");
+            sb.append(minute);
+            sb.append("/1,");
+            sb.append(second);
+            sb.append("/1000,");
+            return sb.toString();
+        }
+    }
+}
+
+
+
+
+
+
